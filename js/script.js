ca76f4c32c7641ab454ee358edc91f3b4d3d60b1
Shivam <shiv240898@gmail.com>
2022-12-30 10:50:16 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 let nextBtn = document.getElementById("next_btn");
-        let changePage = document.getElementById("changePage");
-        nextBtn.addEventListener("click", ()=>{
-            let list1 = document.getElementById("list");
-            values = list1.value;
-            if (values == "fighter") {
-                changePage.href = "fighter.html"
-            }
-            else{
-                changePage.href = "register.html"
-            }
-        })
+let changePage = document.getElementById("changePage");
+let selectValue = document.getElementById("select_value")
+nextBtn.addEventListener("click", () => {
+    let list1 = document.getElementById("list");
+    values = list1.value;
+    if (values == "") {
+        // selectValue.innerHTML = "Please select your role"
+        alert("Please select your role")
+    }
+    else if (values == "fighter") {
+        changePage.href = "fighter.html"
+    }
+    else {
+        changePage.href = "register.html"
+    }
+})
 
